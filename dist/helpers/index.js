a07b1cb0e2b14aca2d3afb9e4573f270bc6b5b29
Alexandre COSTA MOREIRA <alexcostamoreira.pro@gmail.com>
2021-11-04 10:15:18 +0100

    Delete dist folder

--- dist/helpers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var authMiddleware_1 = require("./authMiddleware");
-Object.defineProperty(exports, "authentication", { enumerable: true, get: function () { return authMiddleware_1.default; } });
-var errorHandler_1 = require("./errorHandler");
-Object.defineProperty(exports, "errorHandler", { enumerable: true, get: function () { return errorHandler_1.default; } });
-var userIdMiddleware_1 = require("./userIdMiddleware");
-Object.defineProperty(exports, "userIdCheck", { enumerable: true, get: function () { return userIdMiddleware_1.default; } });
--- dist/services/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoHandler_1 = require("./mongoHandler");
-Object.defineProperty(exports, "MongoHandler", { enumerable: true, get: function () { return mongoHandler_1.default; } });
-var authService_1 = require("./authService");
-Object.defineProperty(exports, "AuthService", { enumerable: true, get: function () { return authService_1.default; } });
